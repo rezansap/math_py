89be60f861e97ff1638ff1d8d176e7df3a43ba26
srezaputra14@gmail.com <srezaputra14@gmail.com>
2021-10-26 16:19:47 +0700

    menambahkan file python dengan 2 pecahan

---
+++ math_2pecahan.py
@@ -0,0 +1,9 @@
+import math
+r = float(input("masukkan jari-jari :"))
+
+luas = math.pi*(r*r)
+keliling = 2*math.pi*r
+
+print ("luas lingkaran \t\t= ",format(luas,'.2f'))
+print ("keliling lingkaran\t= ",format(keliling,'.2f'))
+
